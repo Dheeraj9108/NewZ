2f66bd801435f98965431bf30fc429f693adb407
Dheeraj <poojarydheeraj665@gmail.com>
2022-04-29 19:49:36 +0530

    vid25(2)

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,35 @@ import React, { Component } from 'react'
 import Newsitem from './Newsitem'
 
 export class News extends Component {
+  articles = [
+    {
+      "source" : "gsjdf",
+      "author" : "Dhuru",
+      "title" : "Cricket",
+      "description" : "India won",
+      "url" : "https://hellowworld",
+      "urlToImage" : "sdfgukg",
+      "content" : "India won with westIndies and world cup byu 28 runs for that i have celebrated pary at 12.00" 
+    },
+    {
+      "source" : "gsjdf",
+      "author" : "Dhuru",
+      "title" : "Cricket",
+      "description" : "Russia won",
+      "url" : "https://hellowworldu",
+      "urlToImage" : "sdfgukg",
+      "content" : "India won with westIndies and world cup byu 28 runs for that i have celebrated pary at 12.00" 
+    },
+    {
+      "source" : "gsjdf",
+      "author" : "Dhuru",
+      "title" : "Cricket",
+      "description" : "Russia won",
+      "url" : "https://hellowworldue",
+      "urlToImage" : "sdfgukg",
+      "content" : "India won with westIndies and world cup byu 28 runs for that i have celebrated pary at 12.00" 
+    }
+  ]
   constructor(){
     super();
     this.state = {
@@ -13,15 +42,12 @@ export class News extends Component {
       <div className='container'>
         <h2>NewZee - Top Headlines</h2>
         <div className="row">
-            <div className="col-md-4">
-              <Newsitem title = "myTitel" desc = "my desc" imgUrl = "" newsUrl = ""/>
-            </div>
-            <div className="col-md-4">
-              <Newsitem title = "myTitel" desc = "my desc"/>
-            </div>
-            <div className="col-md-4">
-              <Newsitem title = "myTitel" desc = "my desc"/>
-            </div>
+          {this.state.articles.map((element)=>{
+             return <div className="col-md-4" key={element.url}>
+             <Newsitem title = {element.title.slice(0,45)} desc = {element.description.slice(0,88)} imgUrl = {element.urlToImage} newsUrl = {element.url}/>
+           </div>
+          })}
+           
         </div>
        
       </div>
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -9,8 +9,8 @@ export class Newsitem extends Component {
         <div className="card" style={{width: "18rem"}}>
             <img src={imgUrl} className="card-img-top" alt=""/>
             <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className="card-text">{desc}</p>
+                <h5 className="card-title">{title}...</h5>
+                <p className="card-text">{desc}...</p>
                 <a href={newsUrl} className="btn btn-sm btn-primary">Read More</a>
             </div>
         </div>
