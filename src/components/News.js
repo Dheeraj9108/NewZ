d77d700300a6d5fbeb713cb53737066c6b06391d
Dheeraj <poojarydheeraj665@gmail.com>
2022-04-29 18:49:39 +0530

    day25

--- src/components/News.js
+++ src/components/News.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react'
 import Newsitem from './Newsitem'
 
 export class News extends Component {
+  constructor(){
+    super();
+    this.state = {
+      articles:this.articles
+    }
+  }
   render() {
     return (
       <div className='container'>
         <h2>NewZee - Top Headlines</h2>
         <div className="row">
             <div className="col-md-4">
-              <Newsitem title = "myTitel" desc = "my desc"/>
+              <Newsitem title = "myTitel" desc = "my desc" imgUrl = "" newsUrl = ""/>
             </div>
             <div className="col-md-4">
               <Newsitem title = "myTitel" desc = "my desc"/>
--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 
 export class Newsitem extends Component {
+ 
   render() {
-    let {title,desc}  = this.props;
+    let {title,desc,imgUrl,newsUrl}  = this.props;
     return (
-      <div>
+      <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-            <img src=" " className="card-img-top" alt=""/>
+            <img src={imgUrl} className="card-img-top" alt=""/>
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{desc}</p>
-                <a href="/" className="btn btn-primary">Go somewhere</a>
+                <a href={newsUrl} className="btn btn-sm btn-primary">Read More</a>
             </div>
         </div>
       </div>
